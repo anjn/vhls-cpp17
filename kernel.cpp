1fa276be28ba09814164fe379e3f222a114e652a
Jun Ando <juna@xilinx.com>
2020-06-05 23:00:48 +0900

    fold expression

--- kernel.cpp
+++ kernel.cpp
@@ -1,13 +1,20 @@
 #include <tuple>
 
-decltype(auto) calc(float a, float x, float b) {
+template<typename T>
+decltype(auto) calc(T a, T x, T b) {
   return std::make_tuple(a*x, b);
+}
+
+template<typename Args...>
+decltype(auto) add(Args args...) {
+  static_assert(sizeof...(args) > 1);
+  return (args + ...);
 }
 
 void func(float* y, float* x, float a, float b, int n) {
   for (int i = 0; i < n; i++) {
     auto [y0, y1] = calc(a, x[i], b);
-    y[i] = y0 + y1;
+    y[i] = add(y0, y1);
   }
 }
 
